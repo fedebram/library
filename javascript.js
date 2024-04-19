5a45f3e9c09d2959725f592380053868732234fd
fedebram <163430291+fedebram@users.noreply.github.com>
2024-04-20 00:40:18 +0200

    reached fourth point

--- javascript.js
+++ javascript.js
@@ -1,5 +1,23 @@
 const myLibrary = [];
 const container = document.querySelector(".container");
+const form = document.querySelector("form");
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const submitButton = document.querySelector("dialog button");
+let bookCount = 0; 
+
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  let title = document.querySelector("#title").value;
+  myLibrary[bookCount] = new Book(title);
+  dialog.close();
+  displayBooks();
+  bookCount++;
+});
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -8,29 +26,19 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {
-  let i = 0;
-  do {
-    let title = prompt("Insert title:  ")
-    let author = prompt("Insert author: ")
-    let pages = prompt("Insert pages: ")
-    let read = prompt("Have you read the book?: ");
-    myLibrary[i] = new Book(title, author, pages, read);
-    i++;
-  } while (i < 2);
-}
-
 function displayBooks() {
-  //for (let i = 0; i < myLibrary.length; i++) {
-    myLibrary.forEach((book) => {
-      let printBook = document.createElement("div");
-      container.appendChild(printBook);
-      printBook.textContent = "Title: " + book.title + " Author: " + book.author + " Number of pages: " + book.pages + " Book already read: " + book.read;
-    });
+  let printBook = document.createElement("div");
+  container.appendChild(printBook);
+  printBook.textContent = "Title: " + myLibrary[bookCount].title + " Author: " + myLibrary[bookCount].author + " Number of pages: " + myLibrary[bookCount].pages + " Book already read: " + myLibrary[bookCount].read;
+  form.reset();
 }
 
-addBookToLibrary();
-displayBooks();
+
+
+
+
+
+
 
 
 
