fa35fb81f214decdc74334afbdd7f3a37494c368
fedebram <163430291+fedebram@users.noreply.github.com>
2024-04-19 21:22:52 +0200

    reached third step

--- javascript.js
+++ javascript.js
@@ -1,9 +1,40 @@
 const myLibrary = [];
+const container = document.querySelector(".container");
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  let i = 0;
+  do {
+    let title = prompt("Insert title:  ")
+    let author = prompt("Insert author: ")
+    let pages = prompt("Insert pages: ")
+    let read = prompt("Have you read the book?: ");
+    myLibrary[i] = new Book(title, author, pages, read);
+    i++;
+  } while (i < 2);
 }
+
+function displayBooks() {
+  //for (let i = 0; i < myLibrary.length; i++) {
+    myLibrary.forEach((book) => {
+      let printBook = document.createElement("div");
+      container.appendChild(printBook);
+      printBook.textContent = "Title: " + book.title + " Author: " + book.author + " Number of pages: " + book.pages + " Book already read: " + book.read;
+    });
+}
+
+addBookToLibrary();
+displayBooks();
+
+
+
+
+
+
+
