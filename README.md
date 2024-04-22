27de0c546064a4f61250fdb7df77507ab096a1d0
fedebram <163430291+fedebram@users.noreply.github.com>
2024-04-22 13:29:25 +0200

    Project done, almost zero styling

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # library
+Create a web page similar to a library, where the user can store a book reference. 
+
+Each book stored in the page has different properties: 
+-title
+-author
+-page
+-if the user have read the book
+
+Each book is displayed on the screen.
+
+For each book, next to the properties shown, there are a "delete" buttons where the user can delete the book.
--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
 const myLibrary = [];
+let bookCount = 0; //serve as index of object inside myLibrary
 const container = document.querySelector(".container");
 const form = document.querySelector("form");
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
 const submitButton = document.querySelector("dialog button");
-let bookCount = 0; 
 
 showButton.addEventListener("click", () => {
   dialog.showModal();
@@ -13,25 +13,96 @@ showButton.addEventListener("click", () => {
 submitButton.addEventListener("click", (event) => {
   event.preventDefault();
   let title = document.querySelector("#title").value;
-  myLibrary[bookCount] = new Book(title);
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let checkbox = document.querySelector("#checkbox").checked;
+
+  myLibrary[bookCount] = new Book(
+    bookCount,
+    title, 
+    author, 
+    pages, 
+    checkbox ? "Read" : "Not read"
+  );
+  
   dialog.close();
   displayBooks();
   bookCount++;
 });
 
-function Book(title, author, pages, read) {
+
+function Book(id, title, author, pages, read) {
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
 
+Book.prototype.getRead = function () {
+ this.read = "Not Read";
+}
+
+
 function displayBooks() {
   let printBook = document.createElement("div");
   container.appendChild(printBook);
-  printBook.textContent = "Title: " + myLibrary[bookCount].title + " Author: " + myLibrary[bookCount].author + " Number of pages: " + myLibrary[bookCount].pages + " Book already read: " + myLibrary[bookCount].read;
+  printBook.setAttribute("id", `_${bookCount}`);
+  printBook.setAttribute("data-id", `${bookCount}`);
+  printBook.textContent = "Title: " + myLibrary[bookCount].title + " Author: " + myLibrary[bookCount].author + " Number of pages: " + myLibrary[bookCount].pages + " Book already read: ";
+  let readButton = document.createElement("input");
+  readButton.setAttribute("type", "checkbox");
+  readButton.setAttribute("id", "read");
+  readButton.setAttribute("name", "read");
+  printBook.appendChild(readButton);
+  let currentBook = myLibrary[bookCount];
+  if (currentBook.read === "Read") readButton.checked = true;
+  readButton.addEventListener("change", () => {
+    currentBook.getRead();
+  });
+
+  let removeBtn = document.createElement("button");
+  printBook.appendChild(removeBtn);
+
+  removeBtn.textContent = "Remove the book";
+  removeBtn.addEventListener("click", (event) => {
+    let divBook = event.target.parentElement;
+    let bookId = divBook.dataset.id;
+
+    // id preso dall'id dell'element div
+    // var bookId = divBook.id.substring(1);
+
+    // approccio con elemento "azzerato", ma posizione array tenuta
+    // myLibrary[bookId] = null;
+
+    // sintassi 1
+    let idxToRemove = -1;
+    for (let idx = 0; idx < myLibrary.length; idx++) {
+      const book = myLibrary[idx];
+      if (book.id == bookId)
+      {
+        idxToRemove = idx;
+        break;
+      }
+    }
+
+    // sintassi 2
+   // let idxToRemove2 = myLibrary.findIndex(book => book.id == bookId);
+
+    // rimuovo se trovato
+    if (idxToRemove != -1) {
+      // rimuove dall'array
+      myLibrary.splice(idxToRemove, 1);
+      //rimuove dal dom
+      divBook.remove()
+    }
+
+    // let divBook = document.querySelector(`#_${bookCount}`);
+  });
+  
   form.reset();
 }
+
 
 
 
